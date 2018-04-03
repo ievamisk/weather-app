7894f743476b85d9d529108bbe8546fd07e29bd1
ievamisk <ieva.misk@gmail.com>
2018-04-03 14:58:02 +0300

    some fixes

--- weather-app/resources/views/weather.blade.php
+++ weather-app/resources/views/weather.blade.php
@@ -8,7 +8,6 @@
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
     <link href="{{ asset('/css/weather.css') }}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link href="{{ asset('/css/open-iconic/font/css/open-iconic-bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('/css/weather-icons/css/weather-icons.css') }}" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
